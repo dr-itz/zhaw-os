38ff8a71865b8fade003c7735acd9ba1aae71046
Daniel Ritz <daniel.ritz@gmx.ch>
2012-10-01 10:38:49 -0700

    exectest/forktest, modified
    - use poll() to wait instead of while(1), use usleep()

---
+++ W40-parallel-processes/demoexec.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+int main ()
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == 0) {
+		printf("Child %d, executing \"/bin/date\"\n", getpid());
+		execl("/bin/date", "date", NULL);
+
+		printf("This should not happen!\n");
+	}
+
+	printf("Parent waiting for child's dead\n");
+	waitpid(pid, &status, 0);
+	printf("Parent, PID: %d: Child with PID %d just died.\n", getpid(), pid);
+
+	exit(0);
+}
---
+++ W40-parallel-processes/forktest.c
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <poll.h>
+
+int main()
+{
+	pid_t k, child;
+	int p, status, q;
+
+	child = fork();
+	if (child != 0) {
+		for (p = 0; p < 20; p++) {
+			printf("\t\t\tParent, iteration %d, child PID=%d\n", p, child); 
+			usleep(500000);
+		}
+		waitpid(child, &status, 0); 
+		printf("\t\t\tChild died, PID was, %d\n", child);
+
+		/* wait for interrupt ^C */
+		poll(NULL, 0, -1);
+	} else {
+		pid_t pid = getpid();
+		printf("Child PID= %d \n", pid);
+		for (p = 0; p < 10; p++) {
+			printf ("Child PID %d, iteration %d\n", pid, p); 
+			usleep(500000);
+		}
+		exit(1);        
+	}  
+}
